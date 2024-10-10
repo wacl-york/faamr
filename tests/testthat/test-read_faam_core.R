97d14f081c28f920ba6e62bd14e99ca99e36caf3
willdrysdale <willdrysdale@googlemail.com>
2024-10-10 22:07:00 +0100

    move read_nitrates tests to correct file

--- tests/testthat/test-read_faam_core.R
+++ tests/testthat/test-read_faam_core.R
@@ -52,17 +52,3 @@ test_that("read core 1hz v5", {
   
 })
 
-test_that("read nitrates v5", {
-  
-  "tests/testthat/fixtures/read_expected/nitrates/core_faam_20240417_v005_r0_c383_1hz.td.RDS"
-  
-  fpRead = testthat::test_path('fixtures','read_inputs','nitrates','core-nitrates_faam_20190711_v001_r0_c179.td.nc')
-  fpExp = testthat::test_path('fixtures','read_expected','nitrates','core-nitrates_faam_20190711_v001_r0_c179.td.RDS')
-  
-  datExp = readRDS(fpExp)
-  datRead = read_faam_nitrates(fpRead)
-  
-  expect_equal(datRead, datExp)
-  
-  
-})
