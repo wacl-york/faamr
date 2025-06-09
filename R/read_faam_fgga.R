19b366d815a5d383a02c4ade2541ab07bd9863e5
willdrysdale <wsd500@york.ac.uk>
2025-06-09 13:35:27 +0100

    document read_fgga and sort check() for github dependency

--- R/read_faam_fgga.R
+++ R/read_faam_fgga.R
@@ -1,13 +1,23 @@
+#' Read FAAM fgga
+#' 
+#' Reads the nasa ames files produced for the FAAM Fast Greenhouse Gas Analyser
+#' 
+#' @param filepath path to file
+#' 
+#' @author W. S. Drysdale
+#' 
+#' @export
 
-read_faam_fgga = function(path){
-  fgga_header = read_nasa_ames_header(path)
+
+read_faam_fgga = function(filepath){
+  fgga_header = read_nasa_ames_header(filepath)
   
   date_origin = as.POSIXct(fgga_header$long_names[1], format = "Time (seconds since %Y-%m-%d %H:%M:%S UTC)", tz = "UTC")
   
   fgga_header$long_names[1] = "date"
   
-  fgga = nasaAmesR::read_nasa_ames_1001(path) |> 
-    setNames(fgga_header$long_names) |> 
+  fgga = nasaAmesR::read_nasa_ames_1001(filepath) |> 
+    stats::setNames(fgga_header$long_names) |> 
     janitor::clean_names() |> 
     dplyr::mutate(date = date+date_origin |> 
                     nanotime::as.nanotime())
