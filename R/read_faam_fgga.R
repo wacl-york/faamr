5fe21b7fe75b0ddbbeb6c892c932aac51b56e72e
willdrysdale <wsd500@york.ac.uk>
2025-07-28 16:49:34 +0100

    Add extract_fgga_uncert

--- R/read_faam_fgga.R
+++ R/read_faam_fgga.R
@@ -1,26 +1,25 @@
-#' Read FAAM fgga
-#' 
-#' Reads the nasa ames files produced for the FAAM Fast Greenhouse Gas Analyser
-#' 
-#' @param allowExtrapolatedCal mole fraction error flags can take the values 0, 1 and 2.
-#'                             \itemize{
-#'                                \item 0 signifies valid data and is always retained. 
-#'                                \item 1 signifies reduced accuracy data where the calibration has been extrapolated
-#'                                \item 2 signifies missing data, and these are always removed
-#'                                }
-#'                             Setting this value to FALSE removed data where the flag is 1. Default TRUE
-#'                                
-#' @param requireHighFlow The FGGA is capable of 10 Hz measurements, but is required to be in High Flow mode. 
-#'                        This is flagged by the instrument_flow column, where flag == 1 is high flow mode, suitable for 10 Hz data
-#'                        when \code{requireHighFlow == TRUE}, all the data is filtered for flag == 1, otherwise, flag %in% c(0, 1) is allowed
-#'                        default FALSE
-#' 
-#' @inheritParams read_faam_core
-#' 
-#' @author W. S. Drysdale
-#' 
-#' @export
-
+##' Read FAAM fgga
+##' 
+##' Reads the nasa ames files produced for the FAAM Fast Greenhouse Gas Analyser
+##' 
+##' @param allowExtrapolatedCal mole fraction error flags can take the values 0, 1 and 2.
+##'                             \itemize{
+##'                                \item 0 signifies valid data and is always retained. 
+##'                                \item 1 signifies reduced accuracy data where the calibration has been extrapolated
+##'                                \item 2 signifies missing data, and these are always removed
+##'                                }
+##'                             Setting this value to FALSE removed data where the flag is 1. Default TRUE
+##'                                
+##' @param requireHighFlow The FGGA is capable of 10 Hz measurements, but is required to be in High Flow mode. 
+##'                        This is flagged by the instrument_flow column, where flag == 1 is high flow mode, suitable for 10 Hz data
+##'                        when \code{requireHighFlow == TRUE}, all the data is filtered for flag == 1, otherwise, flag %in% c(0, 1) is allowed
+##'                        default FALSE
+##' 
+##' @inheritParams read_faam_core
+##' 
+##' @author W. S. Drysdale
+##' 
+##' @export
 
 read_faam_fgga = function(filepath,
                           allowExtrapolatedCal = TRUE,
@@ -97,4 +96,34 @@ read_faam_fgga = function(filepath,
     dplyr::rename("co2_drymole_ppm" = "co2_value",
                   "ch4_drymole_ppb" = "ch4_value")
   
+}
+
+##' Extract fGGA Uncertainties
+##' 
+##' The FAAM non-core fGGA instrument mena bias and uncertainty are stored in the NASA Ames header normal comments.
+##' This function extracts the values and returns a tibble containing bias and uncertainty for CO2 and CH4.
+##' 
+##' @inheritParams read_faam_core
+##' 
+##' @author W. S. Drysdale
+##' 
+##' @export
+
+extract_fgga_uncert = function(filepath){
+  header = read_nasa_ames_header(filepath)
+  uncertaintiesBegin = grep("The mean biases and 1-sigma overall uncertainties are therefore quoted as:", header$normal_comments)
+  
+  stat = c(header$normal_comments[uncertaintiesBegin+1],header$normal_comments[uncertaintiesBegin+2])
+  
+  tibble::tibble(
+    bias = stat |>  
+      stringr::word(1, sep = "\\+") |> 
+      stringr::str_trim() |> 
+      as.numeric(),
+    uncert = stat |>  
+      stringr::word(2, sep = "\\/") |> 
+      stringr::word(2, sep = "\\s+") |> 
+      as.numeric(),
+    name = c("co2_ppm", "ch4_ppb")
+  )
 }
